2e467555b1acd3e6d856edf3d4d1601f53e0624b
Luis Valencia <luisvalencia@Luiss-MacBook-Pro.local>
2019-02-04 13:59:14 -0800

    done

---
+++ FlixApp/MovieView.swift
@@ -0,0 +1,13 @@
+//
+//  MovieView.swift
+//  FlixApp
+//
+//  Created by Luis Valencia on 1/28/19.
+//  Copyright © 2019 Luis Valencia. All rights reserved.
+//
+
+import UIKit
+
+class MovieView: NSObject {
+
+}
