42f4656bf5014544c3be43b59872b1f9850f1312
Luis Valencia <luisvalencia@Luiss-MacBook-Pro.local>
2019-02-11 13:27:35 -0800

    done part 2

---
+++ FlixApp/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  FlixApp
+//
+//  Created by Luis Valencia on 2/11/19.
+//  Copyright © 2019 Luis Valencia. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
